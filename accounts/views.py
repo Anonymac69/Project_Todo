f7c6fe9fd08efb2624703a70d2681eac6de1d615
Anonymac69 <57042937+Anonymac69@users.noreply.github.com>
2020-02-18 04:27:40 +0100

    login/logout views, template mod

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -7,4 +8,23 @@ def home(request):
 
 
 def login_user(request):
-    return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Hi {username}, You\'re Welcome...')
+            return redirect('home')
+        else:
+            messages.success(
+                request, 'Oops! Something is Wrong, Kindly Try Again...')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'See you some ther time...')
+    return redirect('home')
